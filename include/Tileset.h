ded12a3420a2d570a84c11ff97772f5e85af9430
Hugo Ratiney <hugo.ratiney@gmail.com>
2012-05-02 08:56:11 +0400

    Supprimé libjson et petites corrections  - supprimé libjson, abandonné au profit de rapidjson  - petites corrections sur les maps

---
+++ include/Tileset.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <string>
+#include <SFML/Graphics.hpp>
+#include "JSON.h"
+
+class Tileset
+{
+    public:
+        Tileset(std::string path);
+        virtual ~Tileset();
+
+        sf::Image getImage();
+        sf::IntRect getTerrain(std::string name);
+
+    protected:
+    private:
+        sf::Image img;
+        JSON j;
+};
+
---
+++ src/Tileset.cpp
@@ -0,0 +1,47 @@
+#include "Tileset.h"
+
+#include <string>
+#include "JSON.h"
+
+Tileset::Tileset(std::string path) :
+    j(path)
+{
+    std::cout << "JSON2:" << std::endl;
+
+    std::string tilesetPath = "gfx/";
+    tilesetPath += j["tileset"].GetString();
+
+	printf("tileset path = %s\n", tilesetPath.c_str());
+
+	img.LoadFromFile(tilesetPath);
+}
+
+Tileset::~Tileset()
+{
+    //dtor
+}
+
+sf::Image Tileset::getImage()
+{
+    return img;
+}
+
+// DEVRAIT RETOURNER UNE CASE???
+sf::IntRect Tileset::getTerrain(std::string name)
+{
+    assert(j.HasMember("terrains"));
+
+    const js::Value& terrains = j["terrains"];
+
+    assert(terrains.IsObject());
+/*
+    if (terrains.HasMember(name))
+    {
+        //getCoordsOnTileset(terrains[name]["xpos"], terrains[name]["ypos"]);
+    }
+    else
+    {
+        std::cerr << "le terrain \"" << name << "\" est introuvable." << std::endl;
+    }*/
+}
+
